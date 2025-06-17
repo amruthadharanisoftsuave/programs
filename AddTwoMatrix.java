3c9802376197012e6c54ec8a6841f923c4328191
amruthadharanisoftsuave <amruthadharanisoftsuave@gmail.com>
2025-06-17 17:11:01 +0530

    Update AddTwoMatrix.java

--- AddTwoMatrix.java
+++ AddTwoMatrix.java
@@ -1,7 +1,7 @@
 package test1;
 
 public class AddTwoMatrix {
-	public static void main(String[] args) {
+	public static void main(String[] args) {\\
 
 		int[][] matrix1 = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 		int[][] matrix2 = { { 9, 8, 7 }, { 6, 5, 4 }, { 3, 2, 1 } };
